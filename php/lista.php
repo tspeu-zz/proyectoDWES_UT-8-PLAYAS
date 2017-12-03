d30c3e00c7dfa80801d4bdd13bd7be74b860b4da
tspeu <tenshotseuropa@gmail.com>
2017-12-03 19:38:47 +0000

    add

--- php/lista.php
+++ php/lista.php
@@ -4,31 +4,33 @@
     <meta charset="UTF-8">
     <title>Lista Playas UT_8 DWES JM_Banchero</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <link rel="stylesheet" href="css/font-awesome.min.css">
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/old/bootstrap.min.css" rel="stylesheet">
 
     <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- <link href="css/mdb.min.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" type="text/css" href="css/old/style.css">
     <link rel="stylesheet" type="text/css" href="css/dwes.css">
 
      <!-- Material Design Bootstrap -->
      <!-- <link href="css/mdb.min.css" rel="stylesheet"> -->
     <!-- Latest compiled and minified JavaScript -->
-    <!-- <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script> -->
+    <!-- <script src="js/jquery.min.js"></script> -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
     <!-- SCRIPTS -->
 
     <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <!-- <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script> -->
 
     <!-- Bootstrap dropdown -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- <script type="text/javascript" src="js/popper.min.js"></script> -->
 
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- <script type="text/javascript" src="js/bootstrap.min.js"></script> -->
 
     <!-- MDB core JavaScript -->
     <!-- <script type="text/javascript" src="js/mdb.min.js"></script> -->
@@ -43,7 +45,7 @@
 
 <body >
     <!-- <nav class='navbar navbar-default'>  fixed-top fixed-top scrolling-navbar navbar-expand-lg  scrolling-navbar fixed-top-->
-    <nav class="navbar navbar-dark navbar-expand-lg  ">
+    <nav class="navbar navbar-dark ">
         <div class='container'>
             <!-- <div class='navbar-header'> -->
             <a class='navbar-brand'> <img alt='Brand' src='img/ghost.png'></a>
@@ -52,7 +54,7 @@
                     <span class="navbar-toggler-icon"></span>
             </button> -->
             <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
-            <ul class=' navbar-nav mr-auto'>
+            <ul class='nav navbar-nav'>
                 <li class='nav-item '>
                     <a href='../index.html'> PROYECTO UT-8 JM_B  <span class='sr-only'>(current)</span></a>
                 </li>
@@ -65,17 +67,17 @@
     </nav>
 
 <div class="container container-fluid" >
-         <h1  class="panel panel-primary">Listado de playas</h1>
-    <div> 
-        <form id="form" name="formulario" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+    <h1  class="panel panel-primary">Listado de playas</h1>
+    <!-- <div>  -->
+    <form id="form" name="formulario" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 
-            <div class="panel panel-info tn-group">       
-                <div class="row">
+        <div class="panel panel-info tn-group">       
+            <div class="row">
                 <div class="col-md-4">
                     <h3 style="padding: 2% !important;">Seleccione un Municipio</h3>
                 </div>                  
-                    <div class="col-md-4 ">
-                        <select name="id" id="id" style=" width: 100% !important;
+                <div class="col-md-4 ">
+                    <select name="id" id="id" style=" width: 100% !important;
                         height: 100% !important;padding: 8% !important;
                         font-size: 18px;text-align: center !important;
                         background-color: #EEEEEE;">
@@ -90,34 +92,28 @@
                               $error = $e->getCode();
                               $mensaje = $e->getMessage();
                           }
-                    // $sql = "SELECT idMunicipio,nombreMun FROM familia";
+            
                     $sql= "SELECT idMunicipio,nombreMun FROM municipio";
                     $res = $dwes->query($sql);
                         if($res){
                             $value = $res->fetch();
                             while ($value != null) {
                                 echo "<option value='${value['idMunicipio']}'>";
-                                // if (isset($idSelect) && $idSelect == $value['idMunicipio']){
-                                    // echo " selected='true'";  
-                                // }
-                                
                                 echo htmlentities($value['nombreMun'])."</option>";     
                                 $value  = $res->fetch();
                             }     
-
                         }   
                     ?>   
-                        </select>
-                    </div>
-                    <div class="col-md-4">
-                                     <!--  name="idSelect"  value="idSelect" -->
+                    </select>
+            </div>
+                    <div class="col-md-4">                                
                         <button class="btn btn-success" type="submit" 
-                            style="width: 50% !important;padding: 5% !important;">enviar</button>
+                        style="width: 50% !important;padding: 5% !important;">enviar</button>
                     </div>
-                </div>
-            </div>                 
-        </form>
-    </div>
+          <!-- </div> -->
+       </div>                 
+    </form>
+    <!-- </div> -->
 </div>
         
 
